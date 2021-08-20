771009c3fcdc13f28d1bc62731b137c35c99c2c6
Vladimir Pobortsev <pobortcevv@gmail.com>
2021-08-20 19:06:06 +0300

    Допилин первый экран с адекватным форматированием

---
+++ StatefulScreenExample/Tooling/PhoneNumberTextField.swift
@@ -0,0 +1,9 @@
+//
+//  PhoneNumberTextField.swift
+//  StatefulScreenExample
+//
+//  Created by Vladimir Pobortsev on 20.08.2021.
+//  Copyright © 2021 IgnatyevProd. All rights reserved.
+//
+
+import Foundation
